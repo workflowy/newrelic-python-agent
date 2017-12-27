fb9dda11c0fa865a307f5d914c77638aaa5e6d0c
Todd Stansell <todd@stansell.org>
2017-12-27 10:43:08 -0800

    enhance logging to always add some fields and always log when the plugin ends

--- newrelic_python_agent/plugins/mysql.py
+++ newrelic_python_agent/plugins/mysql.py
@@ -443,12 +443,12 @@ class MySQL(base.Plugin):
                 # support comma-separated list
                 metrics = re.split("\s*,\s*", metrics)
 
-        LOGGER.debug("metrics to collect: %s" % ", ".join(metrics))
+        self.log("debug", "metrics to collect: %s" % ", ".join(metrics))
         for cat in metrics:
             if cat in CATEGORIES:
                 self.add_category_stats(cat, cursor)
             else:
-                LOGGER.warning("%s is not a valid metric category" % cat)
+                self.log('warning', "%s is not a valid metric category" % cat)
 
         if 'newrelic' in metrics:
             self.derive_newrelic_stats()
@@ -467,7 +467,7 @@ class MySQL(base.Plugin):
         if 'SQL' not in conf:
             return
 
-        LOGGER.debug("Collecting stats for %s" % category)
+        self.log('debug', "Collecting stats for %s" % category)
         cursor.execute(conf['SQL'])
 
         # call the self.parse_"parser"_stats" function for each one to get the raw key/value pairs
@@ -576,14 +576,14 @@ class MySQL(base.Plugin):
         :param float value: The value of the metric
         """
         if self.is_number(value):
-            LOGGER.debug("Collected raw metric: %s = %s" % (metric, value))
+            self.log('debug', "Collected raw metric: %s = %s" % (metric, value))
             self.raw_metrics[metric] = value
 
     def derive_newrelic_stats(self):
         """
         Derive all of the custom newrelic metric data from what we've collected.
         """
-        LOGGER.debug("Collecting stats for newrelic")
+        self.log('debug', "Collecting stats for newrelic")
         self.derive_newrelic_volume()
         self.derive_newrelic_throughput()
         self.derive_newrelic_innodb()
@@ -888,35 +888,51 @@ class MySQL(base.Plugin):
         self.raw_metrics = dict()
         try:
             self.connection = self.connect()
+            cursor = self.connection.cursor()
+            self.collect_stats(cursor)
+            cursor.close()
+            self.connection.close()
+            self.add_stats()
         except sql.Error as err:
             if _errno(err) == ER_ACCESS_DENIED_ERROR:
-                LOGGER.error("Something is wrong with your user name or password")
+                self.log('error', "Something is wrong with your user name or password")
             elif _errno(err) == ER_BAD_DB_ERROR:
-                LOGGER.error("Database does not exist")
+                self.log('error', "Database does not exist")
             else:
-                LOGGER.error('Could not connect to %s, skipping stats run: %s',
-                             self.__class__.__name__, err)
-            return
-
-        cursor = self.connection.cursor()
-        self.collect_stats(cursor)
-        cursor.close()
-        self.connection.close()
-        self.add_stats()
-        self.finish()
+                self.log('error', 'Could not connect to %s, skipping stats run: %s' %
+                         (self.__class__.__name__, err))
+        finally:
+            self.finish()
 
     def finish(self):
         """Note the end of the stat collection run and let the user know of any
         errors.
 
         """
+        sev = 'info'
+        desc = 'successful'
+        col = 0
         if not self.derive_values and not self.gauge_values:
-            self.error_message()
+            sev = 'error'
+            desc = 'unsuccessful'
         else:
-            dur = time.time() - self.poll_start_time
-            LOGGER.info('%s poll successful',
-                        self.__class__.__name__,
-                        extra={"target_name": self.config['name'],
-                               "hostname": self.config['host'],
-                               "duration": "%.3f" % dur,
-                               "collected": len(self.derive_values) + len(self.gauge_values)})
+            col = len(self.derive_values) + len(self.gauge_values)
+
+        dur = time.time() - self.poll_start_time
+        self.log(sev,
+                 '%s poll %s' % (self.__class__.__name__, desc),
+                 extra={"duration": "%.3f" % dur,
+                        "collected": col})
+
+    def log(self, sev, msg, extra={}):
+        """Provides a way to always log extra fields in log messages.
+
+        :param string sev: The severity to log as.  This should match a LOGGER function name like
+                           'info', 'warning', 'error', or 'debug'
+        :param string msg: The log message itself.
+        :param dict extra: Extra fields to log.  Useful with the json formatter.
+        :rtype: None
+        """
+        extra['target_name'] = self.config['name']
+        extra['hostname'] = self.config['host']
+        getattr(LOGGER, sev)(msg, extra=extra)
